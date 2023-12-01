995c44f171058baac38e4ae69c991c6fefa1d602
Steven McKeown <steven@pragmaflow.com>
2023-12-01 12:11:28 -0500

    day1

---
+++ day1.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const list = fs.readFileSync('./day1list.txt').toString()
+const codes = list.split('\r\n')
+console.log(codes.reduce((acc, code) => {
+  const numbers = code.split('').filter(c => !isNaN(c)).map(c => +c)
+  let lineValueNumber = 0
+  if (numbers.length)
+    lineValueNumber = parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`)
+  return acc + lineValueNumber
+}, 0))
